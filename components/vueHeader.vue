c1a3d4f544ef6901df1bdfbb92e7dd466761fd80
Usagino <yuki.itoh@front-artgraph.com>
2019-05-05 20:24:02 +0900

    update

--- components/vueHeader.vue
+++ components/vueHeader.vue
@@ -4,7 +4,7 @@ header
     .header__menu
       p.header__menu--link SHOP
       p.header__menu--link ABOUT
-    
+    img.header--logo(src="~/assets/image/logo.svg")
     .header__icons
 </template>
 
